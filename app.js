5c4f1908dd0ff0ca3d09157da55ab6c805c4345d
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-19 09:35:49 +0200

    adding website template made before

---
+++ app.js
@@ -0,0 +1,40 @@
+//select element function
+const selectElement = function(element){
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function(){
+    body.classList.toggle('open');
+});
+
+//scroll reveal
+window.sr = ScrollReveal();
+sr.reveal('.animate-left',{
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+window.sr = ScrollReveal();
+sr.reveal('.animate-right',{
+    origin: 'right',
+    duration: 1500,
+    distance: '40rem',
+    delay: 600
+});
+window.sr = ScrollReveal();
+sr.reveal('.animate-top',{
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+sr.reveal('.animate-bottom',{
+    origin: 'bottom',
+    duration: '1000',
+    distance: '25rem',
+    delay: 600
+});
